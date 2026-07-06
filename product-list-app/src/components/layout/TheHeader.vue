9ae87b8c763c04cf2cdc5f1448b8b4737ab5956f
nadirbashir <nadirbashir.pro@gmail.com>
2026-07-06 21:06:30 +0500

    add home button

--- product-list-app/src/components/layout/TheHeader.vue
+++ product-list-app/src/components/layout/TheHeader.vue
@@ -8,10 +8,10 @@
       </h1>
       <ul>
         <li>
-          <router-link to="/products">All Products</router-link>
+          <router-link to="/">Home</router-link>
         </li>
         <li>
-          <router-link to="/cart">Cart</router-link>
+          <router-link to="/products">All Products</router-link>
         </li>
       </ul>
     </nav>
